c68947a7d44b7b968985098b1a948ef554f6b96b
vinitaco <vincent.bergeron.13@ulaval.ca>
2022-02-09 12:59:41 -0500

    Changement lib pour encodeurs sur hardware

--- lib/MotorDriver/MotorDriver.cpp
+++ lib/MotorDriver/MotorDriver.cpp
@@ -1,11 +1,9 @@
 #include "MotorDriver.h"
 
-MotorDriver::MotorDriver(uint8_t pin_1, uint8_t pin_2, uint8_t pwm_pin, uint8_t encoder_pin, volatile uint64_t &encoder_counter) :
+MotorDriver::MotorDriver(uint8_t pin_1, uint8_t pin_2, uint8_t pwm_pin, uint8_t encoder_pin, QuadEncoder &quad_encoder) :
     _pin_1(pin_1),
     _pwm_pin(pwm_pin),
     _pin_2(pin_2),
-    _encoder_pin(encoder_pin),
-    _encoder_counter(encoder_counter),
     _previous_time(micros()),
     _previous_encoder_counter(0)
     {}
@@ -15,7 +13,6 @@ void MotorDriver::initialize(void) {
     pinMode(this->_pin_1, OUTPUT);
     pinMode(this->_pin_2, OUTPUT);
     pinMode(this->_pwm_pin, OUTPUT);
-    pinMode(this->_encoder_pin, INPUT);
 
     analogWriteFrequency(this->_pwm_pin, 60);
 }
@@ -27,11 +24,10 @@ void MotorDriver::setPWM(uint8_t pwm_input) {
 float MotorDriver::getSpeed_Hz(void) {
     uint32_t time = micros();
     uint32_t time_delta = time - this->_previous_time;
-    uint32_t count_delta = this->_encoder_counter - this->_previous_encoder_counter;
+    uint32_t count_delta = this->_quad_encoder.getHoldDifference();
 
     this->_previous_time = time;
-    this->_previous_encoder_counter = this->_encoder_counter;
-
+    
     return ((float)(1e6 * count_delta)) / ((float)time_delta) / 6533.0f;
 }
 
--- lib/MotorDriver/MotorDriver.h
+++ lib/MotorDriver/MotorDriver.h
@@ -2,10 +2,11 @@
 #define __MOTOR_DRIVER_H__
 
 #include <Arduino.h>
+#include "QuadEncoder.h"
 
 class MotorDriver {
     public:
-        MotorDriver(uint8_t pin_1, uint8_t pin_2, uint8_t pwm_pin, uint8_t encoder_pin, volatile uint64_t &encoder_counter);
+        MotorDriver(uint8_t pin_1, uint8_t pin_2, uint8_t pwm_pin, uint8_t encoder_pin, QuadEncoder &quad_encoder);
 
         void initialize(void);
 
@@ -19,8 +20,7 @@ class MotorDriver {
         uint8_t _pin_1;
         uint8_t _pwm_pin;
         uint8_t _pin_2;
-        uint8_t _encoder_pin;
-        uint64_t _encoder_counter;
+        QuadEncoder &_quad_encoder;
         uint32_t _previous_time;
         uint64_t _previous_encoder_counter;
 };
