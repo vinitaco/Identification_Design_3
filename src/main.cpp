72b678f31ec00880c47d5ef93faf9fbf8b01822c
vinitaco <vincent.bergeron.13@ulaval.ca>
2022-02-09 13:26:29 -0500

    Tester l'actionneur

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,32 @@
-#include <Arduino.h>
+#include <Stepper.h>
 
-#include "MotorDriver.h"
-#include "MotorDefines.h"
+const uint8_t STEPPER_PIN_1 = 22;
+const uint8_t STEPPER_PIN_2 = 23;
+const uint8_t STEPPER_PIN_3 = 9;
+const uint8_t STEPPER_PIN_4 = 10;
 
 
-QuadEncoder motor_1_encoder(1, MOTOR_1_PIN_ENCODER, MOTOR_1_PIN_ENCODER, 0);
+const int stepsPerRevolution = 3300;  // change this to fit the number of steps per revolution
+// for your motor
 
 
-MotorDriver motor_1 = MotorDriver(MOTOR_1_PIN_1, MOTOR_1_PIN_2, MOTOR_1_PIN_PWM, MOTOR_1_PIN_ENCODER, motor_1_encoder);
-//MotorDriver motor_2 = MotorDriver(MOTOR_2_PIN_1, MOTOR_2_PIN_2, MOTOR_2_PIN_PWM, MOTOR_2_PIN_ENCODER, motor_2_encoder_counter);
-//MotorDriver motor_3 = MotorDriver(MOTOR_3_PIN_1, MOTOR_3_PIN_2, MOTOR_3_PIN_PWM, MOTOR_3_PIN_ENCODER, motor_3_encoder_counter);
-//MotorDriver motor_4 = MotorDriver(MOTOR_4_PIN_1, MOTOR_4_PIN_2, MOTOR_4_PIN_PWM, MOTOR_4_PIN_ENCODER, motor_4_encoder_counter);
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(stepsPerRevolution, STEPPER_PIN_1, STEPPER_PIN_2, STEPPER_PIN_3, STEPPER_PIN_4);
+
+int stepCount = 0;  // number of steps the motor has taken
 
 void setup() {
-    
-    motor_1_encoder.setInitConfig();
-    motor_1_encoder.EncConfig.IndexTrigger = ENABLE;  //enable to  use index counter
-    motor_1_encoder.EncConfig.INDEXTriggerMode = RISING_EDGE;
-    motor_1_encoder.init();
-
-    motor_1.initialize();
-
-    motor_1.setDirection(true);
-
-    Serial.begin(9600);
-
-    for(uint8_t i = 0; i < 255; ++i) {
-        motor_1.setPWM(i);
-        Serial.print("1:"); Serial.print(motor_1.getSpeed_Hz());
-        delay(100);
-    }
-    delay(2000);
-
-    for(uint8_t i = 255; i > 1; --i) {
-        motor_1.setPWM(i);        
-        Serial.print("1:"); Serial.print(motor_1.getSpeed_Hz());
-        delay(100);
-    }
-
-    motor_1.setPWM(0);
+  // nothing to do inside the setup
 }
 
 void loop() {
+  int motorSpeed = 1023;
+  // set the motor speed:
+  if (motorSpeed > 0) {
+    myStepper.setSpeed(motorSpeed);
+    // step 1/100 of a revolution:
+    myStepper.step(stepsPerRevolution);
+    delay(500);
+    myStepper.step(-stepsPerRevolution);
+  }
 }
